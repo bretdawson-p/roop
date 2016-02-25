3e53ba4146f7138f26da2341a79340dacb7399ec
admin <bret.dawson@interfaceware.com>
2016-02-25 15:30:42 +0000

    Export from PRETTY IMPRESSIVE IGUANA: GUMGUM

--- 1145539D867C4E69553AEF9771E98111/main.lua
+++ 1145539D867C4E69553AEF9771E98111/main.lua
@@ -1,8 +1,10 @@
--- Juicy Fruit Gum!!!!
-
---gum is fun!
+-- HELLO DO YOU LIKE GUM
+-- gum is fun!
 
 function main()
+   
+   
+   
    local sharedfiles = {['shared/well_hello_there.lua']= {} }
    local collation = net.http.get{url='http://localhost:6543/sc/collate',
       auth={username='admin', password='password'},
